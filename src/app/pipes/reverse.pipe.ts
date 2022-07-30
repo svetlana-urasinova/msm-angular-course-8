465baf223e1d6695c68062dee945c9f4c3bce669
Svetlana Urasinova <svetlana.urasinova@ottonova.de>
2022-07-30 23:00:27 +0200

    Replace map with reverse

--- src/app/pipes/reverse.pipe.ts
+++ src/app/pipes/reverse.pipe.ts
@@ -5,9 +5,6 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class ReversePipe implements PipeTransform {
   transform(value: string): string {
-    return value
-      .split("")
-      .map((symbol: string, index: number) => value[value.length - index - 1])
-      .join("");
+    return value.split("").reverse().join("");
   }
 }
